802199cb721ef1fd0b6fc3404634d5fa9601a087
HasnainMurtaza265 <hasnainmurtaza265@gmail.com>
2026-06-09 21:17:47 +0500

    UI completed but not responsive

---
+++ app.js
@@ -0,0 +1,13 @@
+var user_input = document.getElementById("user_input");
+
+var js_number = Math.floor(Math.random()*10+1);
+
+function random_num(){
+
+}
+
+function user() {
+    user_input = user_input.value;
+    // console.log(user_input);
+
+}
