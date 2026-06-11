703cb0af85c1cb286b7cbfb5351d0302d5f74f39
HasnainMurtaza265 <hasnainmurtaza265@gmail.com>
2026-06-11 16:03:50 +0500

    Add guess range checks

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
-var user_input = document.getElementById("user_input");
+var demo = document.getElementById("demo");
+// demo = demo.innerHTML;
 
-var js_number = Math.floor(Math.random()*10+1);
+// console.log(userInput);
+var userInput = document.getElementById("userInput")
 
-function random_num(){
+// JS number Generator
+var botNum = Math.floor(Math.random() * 10 + 1);
 
-}
+// console.log(botNum);
+
 
 function user() {
-    user_input = user_input.value;
-    // console.log(user_input);
+    var userValue = userInput.value;
+    // userInput = +userInput.value;
+    // console.log(userValue);
+
+
+    // while (userValue.length <= 0) {  }
+
+    if (userValue < 1 || userValue > 10) {
+        alert("Please enter a number between 1 and 10");
+    }
+    else {
+        demo.innerHTML = botNum;
+        if (userValue == botNum) {
+            alert("you win");
+            demo.innerHTML = botNum;
+        } else {
+
+            alert("try again")
+        }
+
+
+
+
+    }
 
 }
+// var user_input = document.getElementById("user_input");
+
+// var js_number = Math.floor(Math.random()*10+1);
+
+// function random_num(){
+
+// }
+
+// function user() {
+//     user_input = user_input.value;
+
+// }
+
